e5d700f6b0b2f71b12d70c7b6c3d744440578a5d
ellenai <ellenai@umich.edu>
2023-12-05 23:13:56 -0500

    update the structure

---
+++ Inputter/Func2Encode/build/inputter/CMakeFiles/inputter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inputter.dir/inputter.cpp.o"
+  "CMakeFiles/inputter.dir/inputter.cpp.o.d"
+  "inputter.pdb"
+  "inputter.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inputter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
