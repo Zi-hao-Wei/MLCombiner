cfdf16b7ba4b52503fd45912b7cad3b97a29e81f
ellenai <ellenai@umich.edu>
2023-12-05 23:17:28 -0500

    delete the repeated files

--- Inputter/build/inputter/CMakeFiles/install-inputter.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-inputter"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-inputter.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
